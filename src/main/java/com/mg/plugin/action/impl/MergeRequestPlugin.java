6f3123d0153286985b08c27c76d59e9d0c2432e2
mogupta <mohak.gupta@soprabanking.com>
2019-08-11 13:20:13 +0530

    Successful plugin developed with button dialog panel

--- src/main/java/com/mg/plugin/action/impl/MergeRequestPlugin.java
+++ src/main/java/com/mg/plugin/action/impl/MergeRequestPlugin.java
@@ -3,14 +3,7 @@ package com.mg.plugin.action.impl;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.Messages;
-import com.intellij.util.ui.JBUI;
 import com.mg.plugin.dialog.GitMRDialog;
-
-import javax.swing.*;
-
-import static com.intellij.openapi.ui.Messages.CANCEL_BUTTON;
-import static com.intellij.openapi.ui.Messages.OK_BUTTON;
 
 public class MergeRequestPlugin extends AnAction {
 
--- src/main/java/com/mg/plugin/dialog/GitMRDialog.java
+++ src/main/java/com/mg/plugin/dialog/GitMRDialog.java
@@ -2,25 +2,23 @@ package com.mg.plugin.dialog;
 
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.messages.MessageDialog;
-import org.jetbrains.annotations.Nls;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class GitMRDialog extends MessageDialog {
 
     private JPanel panelWrapper;
+    private static String[] optionsUser = {};
 
     public GitMRDialog(@Nullable Project project, boolean canBeParent) {
-        super(project, canBeParent);
-        init();
+        super(project, "", "", optionsUser, 1, null, canBeParent);
     }
 
     @Override
     protected JComponent createCenterPanel() {
         super.createCenterPanel();
+        panelWrapper = new JPanel();
         JButton jButton = new JButton("This is my button");
         panelWrapper.add(jButton);
         return panelWrapper;
